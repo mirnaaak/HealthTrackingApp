e48a5a35df6713797d33d9d69846c86c20ad26ea
Tala-Cherkawi <cherkawitala@gmail.com>
2024-11-16 01:35:06 +0200

    layout1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
