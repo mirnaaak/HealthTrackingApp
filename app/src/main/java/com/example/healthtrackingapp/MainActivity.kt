c3bafcd92c0341f3b7ea2b799935553b2fb125f6
mirnaaak <mirnaalkhatib1@gmail.com>
2024-11-17 17:18:37 +0200

    history+minor-style-edits

--- app/src/main/java/com/example/healthtrackingapp/MainActivity.kt
+++ app/src/main/java/com/example/healthtrackingapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -20,6 +21,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout1)
+
+        val historyBtn: TextView = findViewById(R.id.historyButton)
+        historyBtn.setOnClickListener{
+            val intent = Intent(this, history::class.java)
+            startActivity(intent)
+        }
 
         val settingBtn: ImageButton = findViewById(R.id.settingsButton)
         settingBtn.setOnClickListener{
---
+++ app/src/main/java/com/example/healthtrackingapp/history.kt
@@ -0,0 +1,70 @@
+package com.example.healthtrackingapp
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePickerColors
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.healthtrackingapp.ui.theme.HealthTrackingAppTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class history : ComponentActivity() {
+    private val calendar = Calendar.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.historylayout)
+
+        val backBtn: ImageView = findViewById(R.id.backArrowImageView)
+
+
+        backBtn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val fromDate: EditText =findViewById(R.id.fromDate)
+        fromDate.setOnClickListener{
+            showDatePicker(fromDate)
+        }
+
+        val toDate: EditText =findViewById(R.id.toDate)
+        toDate.setOnClickListener{
+            showDatePicker(toDate)
+        }
+    }
+
+    private fun showDatePicker(item:EditText) {
+        val datePicker= DatePickerDialog(this,{DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+            val selectedDate:Calendar=Calendar.getInstance()
+            selectedDate.set(year,monthOfYear,dayOfMonth)
+            val dateFormat=SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val formattedDate:String=dateFormat.format(selectedDate.time)
+            item.setText(formattedDate)
+        },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+}
